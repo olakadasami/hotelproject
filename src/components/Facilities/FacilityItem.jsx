4ff9f5782f82fb42f342c1337f6c304b6871b786
Samuel Olakada <olakadasamuel@gmail.com>
2022-09-21 13:15:23 +0100

    minor changes

--- src/components/Facilities/FacilityItem.jsx
+++ src/components/Facilities/FacilityItem.jsx
@@ -5,7 +5,7 @@ function FacilityItem({ item }) {
             <div className="w-full">
                 <img className="w-full" src={item.image} alt="img" />
             </div>
-            <p className='flex justify-center items-center font-bold -mt-10 md:-mt-16 uppercase bg-light text-md md:text-3xl w-48 md:w-72 h-10 md:h-16'>
+            <p className='flex justify-center items-center font-bold -mt-10 md:-mt-16 uppercase bg-light text-md md:text-3xl w-48 md:w-80 h-10 md:h-16'>
                 {item.text}
             </p>
 
